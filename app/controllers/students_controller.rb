379f8c5405d65121fcb2c50abff03f3b6431fe91
vishnu <vishnu.kv@kreatio.com>
2022-07-20 17:28:14 +0530

    add create student view and save in db and password is set in a special pattern

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_user, only: [:show]
+  # before_action :student_params, only: [:create]
 
 
   def index
@@ -7,14 +8,27 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
+  end
 
+  def create
+    @student = Student.new(student_params)
+    debugger
+    password = @student.first_name[0,2].upcase + @student.last_name[0,2].upcase + @student.date_of_birth.year.to_s
+    @student.password = password
+    if @student.save
+      flash.notice = "student created"
+      redirect_to students_path(@student)
+    else
+      flash.alert = "error"
+      redirect_to new_student_path
+    end
   end
 
   def show
   end
 
   def edit
-
   end
 
 
@@ -22,6 +36,10 @@ class StudentsController < ApplicationController
 
   def set_user
     @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :address, :email, :contact_number, :mother_name, :father_name, :date_of_birth)
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
   has_many :user_students
   has_many :users, through: :user_students
